f1f0e38214a892a60ff810f6ef5931d82115141c
denzkie18 <90649171+denzkie18@users.noreply.github.com>
2022-02-11 03:35:43 +0800

    Add files via upload

--- fnovels_item.js
+++ fnovels_item.js
@@ -19,9 +19,11 @@ $(document).ready(function(){
        },{
          title:"How Could You Like Another Girl When You Already Have A Cute Fiancée Like Me?", image:/*"image_storage/How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.jpg"*/"https://dl.dropbox.com/s/lip430whs6or84t/how-could-you-like-another-girl-when-you-already-have-a-cute-fiance-like-me.jpg?dl=0",link:"How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
        },{
-         title:"Omiai Shitakunakattanode, Muri Nandai Na Jouken Wo Tsuketara Doukyuusei Ga Kita Ken Ni Tsuite", image:/*"image_storage/Omiai Shitakunakattanode, Muri Nandai na Jouken wo Tsuketara Doukyuusei ga Kita Ken ni Tsuite.jpg"*/"https://cdn.novelupdates.com/images/2020/11/9fo51lu1frqi9b8yimz4j47wageu_kjp_zb_1cz_gldm.jpg",link:"How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
+         title:"Omiai Shitakunakattanode, Muri Nandai Na Jouken Wo Tsuketara Doukyuusei Ga Kita Ken Ni Tsuite", image:/*"image_storage/Omiai Shitakunakattanode, Muri Nandai na Jouken wo Tsuketara Doukyuusei ga Kita Ken ni Tsuite.jpg"*/"https://cdn.novelupdates.com/images/2020/11/9fo51lu1frqi9b8yimz4j47wageu_kjp_zb_1cz_gldm.jpg",link:"Omiai Shitakunakattanode Muri Nandai na Jouken wo Tsuketara Doukyuusei ga Kita Ken ni Tsuite.html"
        },{
          title:"My Plain-looking Fiance Is Secretly Sweet With Me", image:/*"image_storage/My-Plainlooking-Fiance-is-Secretly-Sweet-with-Me_1614145093.jpg"*/"https://cdn.novelupdates.com/images/2021/02/My-Plainlooking-Fiance-is-Secretly-Sweet-with-Me_1614145093.jpg",link:"My Plain-looking Fiance is Secretly Sweet with Me.html"
+       },{
+         title: "From Toxic Classmate To Girlfriend Goals Light Novel", image: /*"image_storage/from-toxic-classmate-to-girlfriend-goals.jpg"*/"https://dl.dropbox.com/s/vxpci6zu4tjt0a5/16444373003486878436723595178715.jpg?dl=0", link: "from-toxic-classmate-to-girlfriend-goals-light-novel.html"
        }
     ];
   
