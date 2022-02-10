9f691ddc575debdb4a28003c7ac569806f6d3e7c
denzkie18 <90649171+denzkie18@users.noreply.github.com>
2022-02-10 09:14:51 +0800

    Add files via upload

--- fnovels_item.js
+++ fnovels_item.js
@@ -2,22 +2,26 @@ $(document).ready(function(){
   //List of items
   const items = [
        {
-         title:"Days With My Step-sister", image:"https://cdn.novelupdates.com/images/2021/01/Gimai-Seikatsu.jpg",link:"days with my stepsister light novel.html"
+         title:"Days With My Step-sister", image:/*"image_storage/Gimai-Seikatsu.jpg"*/"https://dl.dropbox.com/s/d7u38rnb77okm05/Gimai-Seikatsu.jpg?dl=0",link:"days with my stepsister light novel.html"
        },
        {
-         title:"My Stepsister Is My Ex-girlfriend", image:"image_storage/my-stepsister-is-my-exgirlfriend.jpg",link:"my-stepsister-is-my-exgirlfriend-light-novel.html"
+         title:"My Stepsister Is My Ex-girlfriend", image:/*"image_storage/my-stepsister-is-my-exgirlfriend.jpg"*/"https://dl.dropbox.com/s/w9gfri7gqr0k2ui/0000_b.jpg?dl=0",link:"my-stepsister-is-my-exgirlfriend-light-novel.html"
        },{
-         title:"I Kiss My Girlfriend\'s Little Sister", image:"image_storage/Cover-1.jpg",link:"I kiss my girlfriends little sister light novel.html"
+         title:"I Kiss My Girlfriend\'s Little Sister", image:/*"image_storage/Cover-1.jpg"*/"https://dl.dropbox.com/s/0bs3t0ec6irjbfd/i%20kiss%20my%20girlfriends%20little%20sister%20cover.jpg?dl=0",link:"I kiss my girlfriends little sister light novel.html"
        },{
-         title:"In The Land Of Leadale", image:"image_storage/In_the_Land_of_Leadale_light_novel_volume_1_cover.jpg",link:""
+         title:"In The Land Of Leadale", image:/*"image_storage/In_the_Land_of_Leadale_light_novel_volume_1_cover.jpg"*/"https://dl.dropbox.com/s/tg64h5mtbytoyzh/in_the_land_of_leadale_light_novel_volume_1_cover.jpg?dl=0",link:"in-the-land-of-leadale-light-novel.html"
        },{
-         title:"Return Of Death", image:"image_storage/Return-from-Death-Volume-1.jpg",link:"return-from-death-light-novel.html"
+         title:"Return Of Death", image:/*"image_storage/Return-from-Death-Volume-1.jpg"*/"https://dl.dropbox.com/s/0f7m1m3yfhynrns/return-from-death-volume-1.jpg?dl=0",link:"return-from-death-light-novel.html"
        },{
-         title:"Our Dating Story: The Experienced You And The Inexperienced Me", image:"image_storage/images.jpeg",link:"our-dating-story-the-experienced-you-and-the-inexperiece-me.html"
+         title:"Our Dating Story: The Experienced You And The Inexperienced Me", image:/*"image_storage/0000_cover-2.jpeg"*/"https://dl.dropbox.com/s/fzjqtpotp9h1t06/our-dating-story-the-experienced-you-and-the-inexperienced-me.jpeg?dl=0",link:"our-dating-story-the-experienced-you-and-the-inexperiece-me.html"
        },{
-         title:"My Girlfriend Cheated on Me With a Senior, so I'm Cheating on Her With His Girlfriend", image:"https://cdn.novelupdates.com/images/2022/01/My-Girlfriend-Cheated-on-Me-With-a-Senior-so-Im-Cheating-on-Her-With-His-Girlfriend-LN.jpg",link:"My-Girlfriend-Cheated-on-Me-With-a-Senior-so-Im-Cheating-on-Her-With-His-Girlfriend-LN.html"
+         title:"My Girlfriend Cheated on Me With a Senior, so I'm Cheating on Her With His Girlfriend", image:/*"image_storage/My-Girlfriend-Cheated-on-Me-With-a-Senior-so-Im-Cheating-on-Her-With-His-Girlfriend-LN.jpg"*/"https://cdn.novelupdates.com/images/2022/01/My-Girlfriend-Cheated-on-Me-With-a-Senior-so-Im-Cheating-on-Her-With-His-Girlfriend-LN.jpg",link:"My-Girlfriend-Cheated-on-Me-With-a-Senior-so-Im-Cheating-on-Her-With-His-Girlfriend-LN.html"
        },{
-         title:"How Could You Like Another Girl When You Already Have A Cute Fiancée Like Me?", image:"https://cdn.novelupdates.com/images/2021/12/How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.jpg",link:"How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
+         title:"How Could You Like Another Girl When You Already Have A Cute Fiancée Like Me?", image:/*"image_storage/How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.jpg"*/"https://dl.dropbox.com/s/lip430whs6or84t/how-could-you-like-another-girl-when-you-already-have-a-cute-fiance-like-me.jpg?dl=0",link:"How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
+       },{
+         title:"Omiai Shitakunakattanode, Muri Nandai Na Jouken Wo Tsuketara Doukyuusei Ga Kita Ken Ni Tsuite", image:/*"image_storage/Omiai Shitakunakattanode, Muri Nandai na Jouken wo Tsuketara Doukyuusei ga Kita Ken ni Tsuite.jpg"*/"https://cdn.novelupdates.com/images/2020/11/9fo51lu1frqi9b8yimz4j47wageu_kjp_zb_1cz_gldm.jpg",link:"How-Could-You-Like-Another-Girl-When-You-Already-Have-A-Cute-Fiance-Like-Me.html"
+       },{
+         title:"My Plain-looking Fiance Is Secretly Sweet With Me", image:/*"image_storage/My-Plainlooking-Fiance-is-Secretly-Sweet-with-Me_1614145093.jpg"*/"https://cdn.novelupdates.com/images/2021/02/My-Plainlooking-Fiance-is-Secretly-Sweet-with-Me_1614145093.jpg",link:"My Plain-looking Fiance is Secretly Sweet with Me.html"
        }
     ];
   
