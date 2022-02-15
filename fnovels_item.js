e8e3773480658c6ef285fd1ef2cf7ee82890105f
denzkie18 <90649171+denzkie18@users.noreply.github.com>
2022-02-16 05:05:14 +0800

    Add files via upload

--- fnovels_item.js
+++ fnovels_item.js
@@ -38,6 +38,8 @@ $(document).ready(function(){
          title:"Love Advice From A Childhood Friend",image:"https://dl.dropbox.com/s/rd9jczdwzsw1ie0/16448722605941967747350090885705.jpg?dl=0",link:"love-advice-from-a-childhood-friend-light-novel.html"
        },{
          title:"You Are the Daughter of My First Love Light Novel", image:"https://dl.dropbox.com/s/wh87nrad03iebwt/16449161930398550328096158866316.jpg?dl=0", link:"you-are-the-daughter-of-my-first-love-light-novel.html"
+       },{
+         title:"Re:Monster Light Novel", image:"https://dl.dropbox.com/s/pctxumvjef7b2gc/16449565729146760920435502061508.jpg?dl=0", link:"re-monster-light-novel.html"
        }/*,{
          title:"Eighty Six Light Novel", image:"image_storage/86__000.jpg",link:"eighty-six-light-novel.html"
        }*/
