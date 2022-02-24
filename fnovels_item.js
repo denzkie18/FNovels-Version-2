1cfd9f2cce80e2e186cf1381865207e50b0eccbf
denzkie18 <90649171+denzkie18@users.noreply.github.com>
2022-02-25 04:47:32 +0800

    Add files via upload

--- fnovels_item.js
+++ fnovels_item.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   //List of items
   const items = [
-     {title:"Liar, Liar Light Novel",image:"https://cdn.novelupdates.com/images/2020/10/Liar-Liar.jpg",link:"liar-liar.html"},
+     {title:"The World’s Finest Assassin Gets Reincarnated in Another World as an Aristocrat",image:"https://cdn.novelupdates.com/images/2019/06/coverImage_3247459.jpg",link:"The-Worlds-Finest-Assassin-Gets-Reincarnated-in-Another-World-as-an-Aristocrat-light-novel.html"},
+     {title:"Liar, Liar Light Novel",image:"https://cdn.novelupdates.com/images/2020/10/Liar-Liar.jpg",link:"liar-liar-light-novel.html"},
       {title:"The Girl I Saved on the Train Turned Out to Be My Childhood Friend", image:"https://cdn.novelupdates.com/images/2020/09/4815604202-417x596-1.jpg", link:"The-Girl-I-Saved-on-the-Train-Turned-Out-to-Be-My-Childhood-Friend.html"},
        {
          title:"Days With My Step-sister", image:/*"image_storage/Gimai-Seikatsu.jpg"*/"https://dl.dropbox.com/s/d7u38rnb77okm05/Gimai-Seikatsu.jpg?dl=0",link:"days with my stepsister light novel.html"
